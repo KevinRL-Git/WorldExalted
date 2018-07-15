4ba23566df8de96aecd59e4131e10ebcb3b663d0
Kevin Raymond-Lamoureux <raymondlamoureux.kevin@gmail.com>
2018-07-15 09:05:07 -0400

    add private class

---
+++ Worldexalted/Worldexalted/PrivateClass/uAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worldexalted.PrivateClass
+{
+    class uAttribute
+    {
+        String mName = "";
+        int mLevelDot = 0;
+
+        public void addOneDot()
+        {
+            mLevelDot += 1;
+        }
+        public void removeOneDot()
+        {
+            mLevelDot -= 1;
+        }
+        public void addDot( int nbDot)
+        {
+            mLevelDot += nbDot;
+        }
+        public void removeDot(int nbDot)
+        {
+            mLevelDot -= nbDot;
+        }
+    }
+
+}
